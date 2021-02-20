7e52df8044b3d4ed4558fbe23e1e1e6c36989afb
ShotAlex <shot.aleks@gmail.com>
2021-02-21 00:46:43 +0300

    feat: add full functions in a game. Main completed

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,25 +4,16 @@ import MainLayout from '../containers/MainLayout';
 import rockImg from './../assets/images/ROCK.png';
 import scissorsImg from './../assets/images/SCISSORS.png';
 import paperImg from './../assets/images/PAPER.png';
-import {ROCK, SCISSORS, PAPER} from "../utils/variables";
 import game from '../store/game'
+import {observer} from "mobx-react-lite";
 
 
 const Game = () => {
-  const [result, setResult] = React.useState(false)
-  React.useEffect(() => {
-    setTimeout(() => {
-      setResult(true)
-    }, 1000)
-  }, [])
-
-
   const choices = {
     ROCK: rockImg,
     SCISSORS: scissorsImg,
     PAPER: paperImg,
   }
-  // YOU WIN, YOU LOSE, DRAW
 
   return (
     <MainLayout>
@@ -34,14 +25,12 @@ const Game = () => {
 
         <section className="game-item">
           <h1 className="game-item__title">{game.title}</h1>
-          {result && (
-            <Link to="/" className="game-item__play-again">PLAY AGAIN</Link>
-          )}
+          <Link to="/" className="game-item__play-again">PLAY AGAIN</Link>
         </section>
 
         <section className="game-item">
           <h2 className="game-item__result">COMPUTER PICKED {game.compChoice}</h2>
-          {result && <img src={choices[game.compChoice]} alt={game.compChoice}/>}
+          <img src={choices[game.compChoice]} alt={game.compChoice}/>
         </section>
 
       </article>
@@ -49,4 +38,4 @@ const Game = () => {
   );
 };
 
-export default Game;
+export default observer(Game);
--- src/store/game.js
+++ src/store/game.js
@@ -1,5 +1,5 @@
-import {action, computed, makeAutoObservable, observable} from 'mobx';
-import {PAPER, ROCK, SCISSORS} from '../utils/variables';
+import { makeAutoObservable } from 'mobx';
+import { DRAW, PAPER, ROCK, SCISSORS, YOU_LOSE, YOU_WIN } from '../utils/variables';
 
 class Game {
   constructor() {
@@ -8,7 +8,7 @@ class Game {
 
   score = 0
   title = 'VS'
-  userChoice = PAPER
+  userChoice = ROCK
   compChoice = SCISSORS
 
   incrementScore() {
@@ -23,21 +23,77 @@ class Game {
     this.userChoice = choice
   }
 
-  setCompChoice () {
+  setTitle(newTitle) {
+    this.title = newTitle
+  }
+
+  setCompChoice() {
     const randNum = Math.floor(Math.random() * 3 + 1)
     switch (randNum) {
       case 1:
         this.compChoice = ROCK;
+        this.setResult(ROCK)
         break;
       case 2:
         this.compChoice = SCISSORS;
+        this.setResult(SCISSORS)
         break;
       case 3:
         this.compChoice = PAPER;
+        this.setResult(PAPER)
         break;
       default:
         this.compChoice = ROCK;
     }
+  }
+
+  setResult(comp) {
+    switch (comp) {
+      case ROCK:
+        if (this.userChoice === SCISSORS) {
+          this.setLose()
+        } else if (this.userChoice === PAPER) {
+          this.setWin()
+        } else {
+          this.setDraw()
+        }
+        break;
+      case SCISSORS:
+        if (this.userChoice === PAPER) {
+          this.setLose()
+        } else if (this.userChoice === ROCK) {
+          this.setWin()
+        } else {
+          this.setDraw()
+        }
+        break;
+      case PAPER:
+        if (this.userChoice === ROCK) {
+          this.setLose()
+        } else if (this.userChoice === SCISSORS) {
+          this.setWin()
+        } else {
+          this.setDraw()
+        }
+        break;
+      default:
+        this.setDraw()
+        break;
+    }
+  }
+
+  setWin() {
+    this.setTitle(YOU_WIN)
+    this.incrementScore()
+  }
+
+  setLose() {
+    this.setTitle(YOU_LOSE)
+    this.decrementScore()
+  }
+
+  setDraw() {
+    this.setTitle(DRAW)
   }
 }
 
