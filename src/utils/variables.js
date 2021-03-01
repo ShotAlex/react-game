90b147653623ffaf600bd383a50fe9ff0c7f4516
ShotAlex <shot.aleks@gmail.com>
2021-03-01 22:48:08 +0300

    refactor: replace " to '

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,13 +1,13 @@
-import rockImg from "../assets/images/ROCK.png";
-import scissorsImg from "../assets/images/SCISSORS.png";
-import paperImg from "../assets/images/PAPER.png";
-import rockImgSimple from "../assets/images/ROCK-2.png";
-import scissorsImgSimple from "../assets/images/SCISSORS-2.png";
-import paperImgSimple from "../assets/images/PAPER-2.png";
+import rockImg from '../assets/images/ROCK.png';
+import scissorsImg from '../assets/images/SCISSORS.png';
+import paperImg from '../assets/images/PAPER.png';
+import rockImgSimple from '../assets/images/ROCK-2.png';
+import scissorsImgSimple from '../assets/images/SCISSORS-2.png';
+import paperImgSimple from '../assets/images/PAPER-2.png';
 
-import musicOne from "../assets/music/back-music-1.mp3";
-import musicTwo from "../assets/music/back-music-2.mp3";
-import musicThree from "../assets/music/back-music-3.mp3";
+import musicOne from '../assets/music/back-music-1.mp3';
+import musicTwo from '../assets/music/back-music-2.mp3';
+import musicThree from '../assets/music/back-music-3.mp3';
 
 
 export const ROCK = 'ROCK';
